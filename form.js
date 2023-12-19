247f7e1f49dfbbe24884f116c4874ec009f4358e
Kevin Fahrenberger <fahrenberger.k@web.de>
2023-12-19 08:41:39 +0100

    Changes in Quiz app

---
+++ form.js
@@ -0,0 +1,38 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector('[data-js="form"]');
+    const questionsContainer = document.querySelector('[data-js="questions-container"]');
+  
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+  
+      const question = document.getElementById("question").value;
+      const answer = document.getElementById("answer").value;
+      const tag = document.getElementById("tag").value;
+  
+
+      const card = document.createElement("div");
+      card.classList.add("card");
+  
+      const questionElement = document.createElement("p");
+      questionElement.textContent = `Question: ${question}`;
+  
+      const answerElement = document.createElement("p");
+      answerElement.textContent = `Answer: ${answer}`;
+  
+      const tagElement = document.createElement("p");
+      tagElement.textContent = `Tag: ${tag}`;
+  
+
+      card.appendChild(questionElement);
+      card.appendChild(answerElement);
+      card.appendChild(tagElement);
+  
+      questionsContainer.appendChild(card);
+  
+
+      form.reset();
+    });
+  });
+  
+  
---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleBookmark() {
+  var bookmarkIcon = document.getElementById('bookmarkButton');
+  bookmarkIcon.classList.toggle('bookmarked');
+}
+
+function toggleAnswer() {
+  var answerDiv = document.getElementById('answer');
+  var answerButton = document.getElementById('answerButton');
+  answerDiv.classList.toggle('hidden');
+  if (answerDiv.classList.contains('hidden')) {
+      answerButton.textContent = 'Show Answer';
+  } else {
+      answerButton.textContent = 'Hide Answer';
+  }
+}
