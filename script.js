8b19c333d45965690b50e3a8da19885659ad1e7e
Kevin Fahrenberger <fahrenberger.k@web.de>
2024-01-14 09:29:38 +0100

    fix: removed in HTML implemented JS code,commented JS code out

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-function toggleBookmark() {
-  var bookmarkIcon = document.getElementById('bookmarkButton');
-  bookmarkIcon.classList.toggle('bookmarked');
-}
+//Old code
+//  function toggleBookmark() {
+//   var bookmarkIcon = document.getElementById('bookmarkButton');
+//   bookmarkIcon.classList.toggle('bookmarked');
+// }
 
-function toggleAnswer() {
-  var answerDiv = document.getElementById('answer');
-  var answerButton = document.getElementById('answerButton');
-  answerDiv.classList.toggle('hidden');
-  if (answerDiv.classList.contains('hidden')) {
-      answerButton.textContent = 'Show Answer';
-  } else {
-      answerButton.textContent = 'Hide Answer';
-  }
-}
+// function toggleAnswer() {
+//   var answerDiv = document.getElementById('answer');
+//   var answerButton = document.getElementById('answerButton');
+//   answerDiv.classList.toggle('hidden');
+//   if (answerDiv.classList.contains('hidden')) {
+//       answerButton.textContent = 'Show Answer';
+//   } else {
+//       answerButton.textContent = 'Hide Answer';
+//   }
+// }
